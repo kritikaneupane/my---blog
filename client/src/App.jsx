1f6e85f70c68b6ce5a254e345bab4e42a4e154b7
Your Name <neupanekritika49@gmail.com>
2024-07-07 15:54:04 +0545

    adding rouer dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      <Login />
-      <Register />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
